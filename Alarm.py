99f73db010c72de01902a44607c8381433f0cff2
Snehalathareddygoguri <155904898+Snehalathareddygoguri@users.noreply.github.com>
2024-03-19 23:17:24 +0530

    Update Alarm.py

--- Alarm.py
+++ Alarm.py
@@ -8,51 +8,69 @@ class AlarmClock:
         self.m = m
         # Providing a title
         m.title("Alarm Clock")
-        #Creating label for date
+        # Creating label for date
         self.date_label = tk.Label(m, text="Set Alarm Date (YYYY-MM-DD):")
         self.date_label.pack()
-        #Creating entry
+        # Creating entry
         self.date_entry = tk.Entry(m)
         self.date_entry.pack()
-        #Creating Time Label
+        # Creating Time Label
         self.time_label = tk.Label(m, text="Set Alarm Time (HH:MM):")
         self.time_label.pack()
-        #Creating another Entry
+        # Creating another Entry
         self.time_entry = tk.Entry(m)
         self.time_entry.pack()
-        #Creating button which creates Alarm for Specified Time
+        # Creating button which creates Alarm for Specified Time
         self.set_button = tk.Button(m, text="Set Alarm", command=self.set_alarm)
         self.set_button.pack()
- #Another function for input and checking
+        # Flag to track whether alarm is set
+        self.is_alarm_set = False  
+        # Variables to store alarm time
+        self.alarm_year = None
+        self.alarm_month = None
+        self.alarm_day = None
+        self.alarm_hour = None
+        self.alarm_minute = None
+    # Another function for input and checking
     def set_alarm(self):
         alarm_date = self.date_entry.get()
         alarm_time = self.time_entry.get()
-        #Opening try block for taking input of date and time
         try:
-            alarm_year, alarm_month, alarm_day = map(int, alarm_date.split('-'))
-            alarm_hour, alarm_minute = map(int, alarm_time.split(':'))
-            #Checks our date and time input with current time and date
+            self.alarm_year, self.alarm_month, self.alarm_day = map(int, alarm_date.split('-'))
+            self.alarm_hour, self.alarm_minute = map(int, alarm_time.split(':'))
             current_time = time.localtime()
             current_year, current_month, current_day = current_time.tm_year, current_time.tm_mon, current_time.tm_mday
             current_hour, current_minute = current_time.tm_hour, current_time.tm_min
-            #If matches alerts as message defined in another def function below
-            if (time.mktime((alarm_year, alarm_month, alarm_day, alarm_hour, alarm_minute, 0, 0, 0, 0)) >
+            # Checks our date and time input with current time and date
+            if (time.mktime((self.alarm_year, self.alarm_month, self.alarm_day, self.alarm_hour, self.alarm_minute, 0, 0, 0, 0)) >
                 time.mktime((current_year, current_month, current_day, current_hour, current_minute, 0, 0, 0, 0))):
-                time_difference_seconds = time.mktime((alarm_year, alarm_month, alarm_day, alarm_hour, alarm_minute, 0, 0, 0, 0)) - time.mktime((current_year, current_month, current_day, current_hour, current_minute, 0, 0, 0, 0))
+                time_difference_seconds = time.mktime((self.alarm_year, self.alarm_month, self.alarm_day, self.alarm_hour, self.alarm_minute, 0, 0, 0, 0)) - time.mktime((current_year, current_month, current_day, current_hour, current_minute, 0, 0, 0, 0))
                 messagebox.showinfo("Alarm Set", f"Alarm set for {alarm_date} {alarm_time}")
-                self.m.after(int(time_difference_seconds), self.alert)
+                self.is_alarm_set = True
+                self.check_alarm()  # Start checking for the alarm
             else:
                 messagebox.showwarning("Invalid Input", "Please enter a future date and time")
         except ValueError:
             messagebox.showerror("Invalid Input", "Please enter a valid date in YYYY-MM-DD and time in HH:MM format")
-    #Alerts if alarm time matches with current time
+    # Alerts if alarm time matches with current time
+    def check_alarm(self):
+        current_time = time.localtime()
+        current_year, current_month, current_day = current_time.tm_year, current_time.tm_mon, current_time.tm_mday
+        current_hour, current_minute = current_time.tm_hour, current_time.tm_min
+        
+        if self.is_alarm_set and (current_year, current_month, current_day, current_hour, current_minute) == (
+                self.alarm_year, self.alarm_month, self.alarm_day, self.alarm_hour, self.alarm_minute):
+            self.alert()
+        else:
+            self.m.after(1000, self.check_alarm)  # Check every second
+    # Alerts if alarm time matches with current time
     def alert(self):
         messagebox.showinfo("Alarm", "Wake up!")
-#Main Function
+# Main Function
 def main():
     root = tk.Tk()
     alarm_clock = AlarmClock(root)
     root.mainloop()
-#Calling main function
+# Calling main function
 if __name__ == "__main__":
     main()
